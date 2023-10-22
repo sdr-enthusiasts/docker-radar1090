b12e7bd56599a8b1f339cb9d29b7276e75e0d86a
kx1t <kx1t@amsat.org>
2023-10-22 11:51:59 -0400

    many updates, add github autobuild actions

---
+++ rootfs/scripts/healthcheck.sh
@@ -0,0 +1,51 @@
+#!/command/with-contenv bash
+# shellcheck shell=bash disable=SC1091
+# SC2015,SC2164,SC2068,SC2145,SC2120
+
+source /scripts/common
+s6wrap=(s6wrap --quiet --timestamps --prepend="$(basename "$0")")
+
+#---------------------------------------------------------------------------------------------
+# This repository, docker container, and accompanying scripts and documentation is
+# Copyright (C) 2022-2023, Ramon F. Kolb (kx1t)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+# Radar1090 is an ADS-B RADAR Feed Service
+# Copyright (C) 2023 by Michael J. Tubby B.Sc. MIET G8TIC mik@tubby.org All Rights Reserved.
+# No license to the "radar" binary and its source code is implied; contact the author for information.
+#---------------------------------------------------------------------------------------------
+
+# Healthcheck for autoheal. Returns 1 if $HEALTHFILE contains a number greater than 0
+# This number is written by the watchdog service to ensure there's traffic flowing for radar1090
+
+HEALTHFILE=/run/watchdog-log/health_failures_since_last_success
+MEASURE_TIME="${MEASURE_TIME:-15}"              # how long we take samples to check data is flowing
+MEASURE_INTERVAL="${MEASURE_INTERVAL:-300}"     # wait time between check runs
+TRANSPORT_PROTOCOL="${TRANSPORT_PROTOCOL:-udp}" # [udp|tcp] the protocol used to transport data to the remote aggregator
+TRANSPORT_PROTOCOL="${TRANSPORT_PROTOCOL,,}"
+
+FAILURES_TO_GO_UNHEALTHY=3  # after this number of failures, the container will go unhealthy
+
+# make sure the log files exists:
+mkdir -p "$(dirname "$HEALTHFILE")"
+touch "$HEALTHFILE"
+
+read -r healthfailures < "$HEALTHFILE"
+
+if [[ -n "$healthfailures" ]] && (( healthfailures > FAILURES_TO_GO_UNHEALTHY )); then
+    "${s6wrap[@]}" --args echo "UNHEALTHY: No data is flowing to ${RADARSERVER:-adsb-in.1090mhz.uk}:${RADARPORT:-2227}/${TRANSPORT_PROTOCOL:-udp} - failure count since last successful measurement is $healthfailures"
+    exit 1
+else
+    exit 0
+fi
